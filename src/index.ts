928b21821157aa680e9fe25904547717b7f6fc06
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2024-05-12 17:17:02 -0300

    [00] - Create express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello, teste!')
+import express,{Express} from 'express';
+
+const app:Express = express();
+const port = 3000 || process.env.PORT;
+
+app.get('',(req:express.Request,res:express.Response)=>{
+    res.send("Hello world")
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
