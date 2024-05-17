7bfdfc6f8e1c8c65fe8e6c64befdcc618211b414
flaviana <flavicastelocc@gmail.com>
2024-05-17 14:17:56 -0300

    repo vazio

--- src/infra/database/useCase/databaseUseCase.ts
+++
@@ -1,8 +0,0 @@
-import { MySQLConfig } from "../mysql/mysql.config";
-
-export interface DatabaseUseCase {
-    connect(config:any): Promise<string| Error>;
-    verifyConnection(connection:any): Promise<string| Error>;
-    disconnect(connection:any): Promise<string| Error>;
-    verifyConnection(connection:any): Promise<string| Error>;
-}
